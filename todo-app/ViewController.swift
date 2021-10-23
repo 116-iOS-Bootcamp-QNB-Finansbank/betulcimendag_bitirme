b3e0990e484dca62071d67515c75d1f82fe8f797
betül çimendağ <cimendag16@itu.edu.tr>
2021-10-23 03:27:44 +0300

    project initialized

---
+++ todo-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todo-app
+//
+//  Created by Betül Çimendağ on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
